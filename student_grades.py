1efce8443607801bb0fdb1616bb4af522144ba53
Sara Kralova <257016@vutbr.cz>
2026-04-22 10:21:12 +0200

    Vyhledávání studentů

--- student_grades.py
+++ student_grades.py
@@ -9,25 +9,32 @@ class StudentsGrades:
         return len(self.scores)
 
     def get_grade(self, index):
-        points = self.get_by_index(index)
+        body = self.get_by_index(index)
 
-        if points >= 90:
+        if body >= 90:
             return "A"
-        elif points >= 80:
+        elif body >= 80:
             return "B"
-        elif points >= 70:
+        elif body >= 70:
             return "C"
-        elif points >= 60:
+        elif body >= 60:
             return "D"
-        elif points >= 50:
+        elif body >= 50:
             return "E"
         else:
             return "F"
+
+    def find(self, value):
+        vysledek = []
+        for i in range(len(self.scores)):
+            if self.scores[i] == value:
+                vysledek.append(i)
+        return vysledek
 
 
 if __name__ == "__main__":
     results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
 
-    print(results.get_grade(2))
-    print(results.get_grade(6))
-    print(results.get_grade(7))
+    print(results.find(100))
+    print(results.find(50))
+    print(results.find(77))
