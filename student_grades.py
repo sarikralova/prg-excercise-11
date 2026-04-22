af952f6f7b429237ff12091039d5fb7dabf12f4e
Sara Kralova <257016@vutbr.cz>
2026-04-22 10:16:29 +0200

    Udělení známky

---
+++ student_grades.py
@@ -0,0 +1,33 @@
+class StudentsGrades:
+    def __init__(self, scores):
+        self.scores = scores
+
+    def get_by_index(self, index):
+        return self.scores[index]
+
+    def count(self):
+        return len(self.scores)
+
+    def get_grade(self, index):
+        points = self.get_by_index(index)
+
+        if points >= 90:
+            return "A"
+        elif points >= 80:
+            return "B"
+        elif points >= 70:
+            return "C"
+        elif points >= 60:
+            return "D"
+        elif points >= 50:
+            return "E"
+        else:
+            return "F"
+
+
+if __name__ == "__main__":
+    results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+
+    print(results.get_grade(2))
+    print(results.get_grade(6))
+    print(results.get_grade(7))
