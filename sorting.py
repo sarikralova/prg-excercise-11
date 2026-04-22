9fb52ca4ea9c36b89d9f05faa01f9d7513c44a1b
Sara Kralova <257016@vutbr.cz>
2026-04-22 09:43:07 +0200

    Vizualizace Bubble Sortu

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -14,21 +15,35 @@ def selection_sort(hodnoty):
         hodnoty[j], hodnoty[min_index] = hodnoty[min_index], hodnoty[j]
     return hodnoty
 
-
 def bubble_sort(cisla):
     cisla = cisla.copy()
+
+    plt.ion()
+    plt.show()
+
     for j in range(len(cisla)):
         for i in range(0, len(cisla) - j - 1):
+
+            index_highlight1 = i
+            index_highlight2 = i + 1
+            colors = ["steelblue"] * len(cisla)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+
+            plt.clf()
+            plt.bar(range(len(cisla)), cisla, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+
             if cisla[i] > cisla[i + 1]:
                 cisla[i], cisla[i + 1] = cisla[i + 1], cisla[i]
+
+    plt.ioff()
+    plt.show()
+
     return cisla
 
 if __name__ == "__main__":
-
-    test = [5, 1, 4, 2, 8]
-    print(test)
-    print(bubble_sort(test))
-    print(test)
 
     hodnoty = random_numbers(20)
     print(hodnoty)
